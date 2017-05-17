ce27f8464c90f764b8997822bb9cd9c4942917c9
pjsalinas <pedrojsalinas@gmail.com>
2017-05-17 11:09:09 -0400

    Add Slack Slash Command and Twilio platforms

---
+++ README.md
@@ -0,0 +1,24 @@
+# PecheBot
+A way to answer questions about my career using different platforms. You don't have to be with me to find out.
+Here is how you can do this:
+
+Slack
+/pechebot 'theQuestion'
+
+Twilio
+Send text to my number
+
+Telegram
+Connect with my bot
+
+Skype
+
+
+### What you will need:
+*Slack*
+Slack token assigned when you set up Slack Slash Command Integration
+
+*Twilio*
+* Twilio Account ID
+* Twilio Auth Token
+* Twilio Number
--- bot.js
+++ bot.js
@@ -1,24 +1,74 @@
-const twilio = require('twilio');
-conslt botBuilder = require('claudia-bot-builder');
+/**
+ * How to have PecheBot answering questions from different platforms
+ */
 
-if(!process.env.TWILIO_AUTH_TOKEN){
-	var env = require('.env');
-}
+"use strict";
 
-const api = botBuilder((request, requestApi) => {
-	if(request.type === 'slack'){
-		backToPlatforms("Slack => ", request.text);
-	} else if (request.type === 'twilio') {
-		backToPlatfoms("Twilio => " + request.text);
-	} else if (request.type === 'skype') {
-		backToPlatforms("Skype => " + request.text);
-	} else if (request.type === 'telegram') {
-		backToPlatforms("Telegram => " + request.text);
-	} else {
-		return "We don't support that platform yet"
+
+const botBuilder = require('claudia-bot-builder');
+const _ = require('lodash');
+
+// Load the file with the answer to the pre-existing questions
+const scriptRules = require('./answer.json');
+
+
+const api = botBuilder((message, originalApiRequest) => {
+
+	// Get the message, trim the blank spaces and convert it to upper case.
+	// answer.json keys are upper case. Avoid the fact the user can type 
+	// lower and upper case letters.
+	var upperText = message.text.trim().toUpperCase();
+
+	// Check if text sent is the key for one of the pre-existing answers.
+	var response = _.get(scriptRules, upperText, "I didn't undestand that. Type 'more' for info.");
+
+	/*
+	originalApiRequest.post = {
+		token: "",
+		team_id: "",
+		team_domain: "pjsalinas",
+		channel_id: "",
+		channel_name: "",
+		user_id: "",
+		user_name: "",
+		command: "/pechebot",
+		text: "bot",
+		response_url: "https://hooks.slack.com/commands/1234/5678"
 	}
-}, platforms: ['slackslashcommand', 'skype', 'twilio', 'telegram');
+	*/
 
-var backToPlatforms = (platform, text) => {
-	return platform + text;
-};
+	if(message.type === 'slack-slash-command') {
+		return {
+			response_type: "in_channel",
+			text: `${response}`,
+			/*
+			attachments: [
+				{
+					text: "url => " + originalApiRequest.post.response_url + '\n' +
+						  "channel_name => " + originalApiRequest.post.channel_name  + '\n' +
+						  "channel_id => " +  originalApiRequest.post.channel_id
+				}
+			]
+			*/
+		}
+	} else if (message.type === 'twilio') {
+		/*
+			This is the info you have to collect in advance:
+		 		Twilio Account ID
+		 		Twilio Auth Token
+		 		Twilio Number
+		*/
+
+		return response;
+	} else {
+		return 'you are coming from a platform we do not support yet.';
+	}
+
+
+
+	// These are the planned platforms so far.
+}, { platforms: ['slackSlashCommand', 'telegram', 'skype', 'twilio'] });
+
+var sendMessageToSlack = () => {};
+
+module.exports = api;
